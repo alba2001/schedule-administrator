2e652ee661cfd20c636ce8e2871d8efee9a0b7f1
Ovcharenko Konstantin <alba2001@meta.ua>
2012-11-10 10:27:44 +0200

    Работа над добавлением даты рожденья преподавателя и отправки SMS

---
+++ controllers/schedule.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Schedule Controller for Schedule Component
+ * 
+ * @package    Training schedule
+ * @subpackage Components
+ * @link http://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_4
+ * @license		GNU/GPL
+ */
+
+// No direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+/**
+ * Schedule Controller
+ *
+ * @package    Training schedule
+ * @subpackage Components
+ */
+class SchedulesControllerShedule extends SchedulesController
+{
+}
